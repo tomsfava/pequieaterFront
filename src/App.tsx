9dd5058af304518e72aeffbc329074325f639e66
Tomás Fava <tomsfava@gmail.com>
2025-06-14 17:00:43 -0300

    adicionando rota de registro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />}></Route>
+                <Route path="/" element={<Login />} />
+                <Route path="/register/" element={<Register />} />
             </Routes>
         </Router>
     )
